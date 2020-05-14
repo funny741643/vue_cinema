d599f923d0d20ce1d02fc41bc7f11ee64358ac4f
姚非凡 <7370372+yao_extraordinary@user.noreply.gitee.com>
2020-05-14 12:50:32 +0800

    项目初始化

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">我是Home主页面</div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login_wrapper">登录界面</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
